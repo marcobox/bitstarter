7582628e585b1c6a57e2f44224eb443d26aad224
MarcoBox <marcoboxdev@gmail.com>
2013-07-18 18:06:23 +0000

    Web.JS now write the content of index.html to stream

--- web.js
+++ web.js
@@ -2,11 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var file = fs.readFileSync("index.html");
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
+		response.send(file.toString());
+	});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+		console.log("Listening on " + port);
+	});
