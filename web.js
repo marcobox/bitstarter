39db12c57de010d3f387fb478aef059de4eedc1e
MarcoBox <marcoboxdev@gmail.com>
2013-07-21 20:23:50 +0000

    Modified index.html for HW4 and added guardfile setting also fixed bug with web.js that was not letting it refresh the content of a page propely

--- web.js
+++ web.js
@@ -3,12 +3,11 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var file = fs.readFileSync("index.html");
 app.get('/', function(request, response) {
-		response.send(file.toString());
+		response.send(fs.readFileSync("index.html").toString());
 	});
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
 		console.log("Listening on " + port);
 	});
